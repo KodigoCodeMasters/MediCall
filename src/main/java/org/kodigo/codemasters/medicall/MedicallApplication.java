6b2d0cda9cc06b9ce5ebe3db7d1e2986b9893bfa
José Marcelo Hernández Cerritos <Jose.marcelo.1706@gmail.com>
2021-09-20 21:33:50 -0600

    Project Initialization

---
+++ src/main/java/org/kodigo/codemasters/medicall/MedicallApplication.java
@@ -0,0 +1,13 @@
+package org.kodigo.codemasters.medicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kodigo/codemasters/medicall/MedicallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kodigo.codemasters.medicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
