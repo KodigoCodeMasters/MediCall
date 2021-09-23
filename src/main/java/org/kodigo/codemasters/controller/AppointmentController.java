b5d1e4f8ed1dd5cc085510fb4e8eb697e8f71b56
nestorrecinosUCA <62679356+nestorrecinosUCA@users.noreply.github.com>
2021-09-23 14:19:40 -0600

    :sparkles: newAppointment page added and Style.css updated

---
+++ src/main/java/org/kodigo/codemasters/controller/AppointmentController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kodigo.codemasters.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Néstor Recinos
+ */
+@Controller
+public class AppointmentController {
+    @RequestMapping("/newappointment")
+    public String newappointment(){
+        return "newappointment";
+    }
+}
