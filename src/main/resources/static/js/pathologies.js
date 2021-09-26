bcace22c68c3c002c4e8c98664d1b163dd63feb9
nestorrecinosUCA <62679356+nestorrecinosUCA@users.noreply.github.com>
2021-09-26 14:10:27 -0600

    :sparkles: counter added to Pathologies

---
+++ src/main/resources/static/js/pathologies.js
@@ -0,0 +1,11 @@
+//Function for counting the number of pathologies
+var numberOfPathologies = 0;
+document.getElementById("num_of_pathologies").innerHTML = numberOfPathologies;
+function countPathologies(id){
+    if(document.getElementById(id).checked){
+        this.numberOfPathologies++;
+    }else{
+        this.numberOfPathologies--;
+    }
+    document.getElementById("num_of_pathologies").innerHTML = numberOfPathologies;
+}
