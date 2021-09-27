d9ea7e249b50357a6275773504cc03c24fe303df
nestorrecinosUCA <62679356+nestorrecinosUCA@users.noreply.github.com>
2021-09-27 10:47:22 -0600

    :fire: some lines deleted from pathologies view and controller

--- src/main/java/org/kodigo/codemasters/controller/doctor/PatienPathologiesController.java
+++ src/main/java/org/kodigo/codemasters/controller/doctor/PatienPathologiesController.java
@@ -16,8 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PatienPathologiesController {
     @RequestMapping("/patient/pathologies")
-    public String patient_pathologies(Model model){
-        model.addAttribute("pathologies");
+    public String patient_pathologies(){
         return "doctor/pathologies_patient";
     }
 }
